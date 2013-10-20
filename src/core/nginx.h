f9a627e9a2db8ba6056842e578bbdb7046733c1d
syl5166 <syl5166@163.com>
2013-10-20 10:19:36 +0800

    first_commit

---
+++ src/core/nginx.h
@@ -0,0 +1,21 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Nginx, Inc.
+ */
+
+
+#ifndef _NGINX_H_INCLUDED_
+#define _NGINX_H_INCLUDED_
+
+
+#define nginx_version      1004002
+#define NGINX_VERSION      "1.4.2"
+#define NGINX_VER          "nginx/" NGINX_VERSION
+
+#define NGINX_VAR          "NGINX"
+#define NGX_OLDPID_EXT     ".oldbin"
+
+#define NGINX_AUTH_MODIFY  " modified by shiyalei"
+
+#endif /* _NGINX_H_INCLUDED_ */
